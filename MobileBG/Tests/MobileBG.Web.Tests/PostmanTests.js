09cb4c9fbf950ecd3e0207e823d0455ffd273bcc
Pavel Josifov <josifov.pavel@gmail.com>
2022-10-05 13:00:46 +0300

    Added Postman tests.

---
+++ MobileBG/Tests/MobileBG.Web.Tests/PostmanTests.js
@@ -0,0 +1,21 @@
+﻿//The URL used for this test is: https://localhost:44319/api/Models/ForMake?makeId=CC63619E-C891-448D-6AD7-08DAA522024B. The tests are runned for the Audi brand.
+
+pm.test("Status test", function () {
+    pm.response.to.have.status(200);
+});
+
+pm.test("Audi_Model test", function () {
+    pm.response.to.have.jsonBody();
+});
+
+pm.test("Body contains string", () => {
+    pm.expect(pm.response.text()).to.include("A3");
+});
+
+pm.test("Body contains string", () => {
+    pm.expect(pm.response.text()).to.include("A5");
+});
+
+pm.test("Body dostn't contain string", () => {
+    pm.expect(pm.response.text()).not.to.include("1895");
+});
